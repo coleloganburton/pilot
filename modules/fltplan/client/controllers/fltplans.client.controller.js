8b6967637bd79d8594f9c3f779e68f370053a248
Cole Burton <cole.l.burton@gmail.com>
2017-03-31 10:40:00 -0400

    Airports feature working, needs formatting

--- modules/fltplan/client/controllers/fltplans.client.controller.js
+++ modules/fltplan/client/controllers/fltplans.client.controller.js
@@ -1,25 +1,19 @@
 'use strict';
 
 // fltplans controller
-angular.module('fltplans').controller('fltplansController', ['$scope', '$http', '$stateParams', '$location', 'Authentication', 'fltplans',
-  function ($scope, $http, $stateParams, $location, Authentication, fltplans) {
+angular.module('fltplans').controller('fltplansController', ['$scope', '$filter','$http', '$stateParams', '$location', 'Authentication', 'fltplans',
+  function ($scope, $filter, $http, $stateParams, $location, Authentication, fltplans) {
 
     $scope.authentication = Authentication;
 
-    //build URL
-    //encodeURIComponent will not encode ~!*()'
-    //escape will not encode @*/+
-    var yahoo = 'https://query.yahooapis.com/v1/public/yql?q=';
-    var select = "select%20*%20from%20html%20where%20url%3D'";
-    var queryURL = encodeURIComponent("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=");
-    var extra = "%26hoursBeforeNow%3D2";
-    var format = "'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-    //var testURL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'https%3A%2F%2Faviationweather.gov%2Fadds%2Fdataserver_current%2Fhttpparam%3FdataSource%3Dmetars%26requestType%3Dretrieve%26format%3Dxml%26stationString%3DKDEN%2520KSEA%2C%2520PHNL%26hoursBeforeNow%3D2'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-    //try encoding urls
+    $scope.airports = {};
 
     $scope.validateRoute = function (route) {
       var input = route.split(" ");
       var isValid = false;
+
+
+      $scope.airports = [];
 
       //format input
       for (var x = 0; x < input.length; x++){
@@ -41,7 +35,15 @@ angular.module('fltplans').controller('fltplansController', ['$scope', '$http', 
           isValid = false;
           break;
         }
+
+        //assign airport to airports
+        $scope.airports.push({
+          ident: input[x],
+          sortable: true,
+          resizable: true
+        });
       }
+      //console.log($scope.airports);
 
       //Form request route
       var requestRoute = input[0] + ",";
@@ -58,12 +60,20 @@ angular.module('fltplans').controller('fltplansController', ['$scope', '$http', 
       }
     };
 
+    //Load Weather and Airport Data
     $scope.submit = function (route) {
 
+      $scope.showBody = true;
 
+      //encodeURIComponent will not encode ~!*()'
+      //escape will not encode @*/+
+      var yahoo = 'https://query.yahooapis.com/v1/public/yql?q=';
+      var select = "select%20*%20from%20html%20where%20url%3D'";
+      var queryURL = encodeURIComponent("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=");
+      var extra = "%26hoursBeforeNow%3D2";
+      var format = "'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
       var RequestURL = yahoo + select + queryURL + encodeURIComponent(route) + extra + format;
-
 
       //GET METARs from YQL
       $http.get(RequestURL)
@@ -105,25 +115,114 @@ angular.module('fltplans').controller('fltplansController', ['$scope', '$http', 
         return (100*(top/btm)).toFixed(0);
       };
 
-
-    /*
-    $scope.density_altitude = function (metar) {
-    };
-    */
-
-
+      /*
+      $scope.density_altitude = function (metar) {
+      };
+      */
 
     });
+
+      //Airports
+
+      //setup queryURL
+      var a ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'https%3A%2F%2Fnfdc.faa.gov%2FnfdcApps%2Fservices%2FairportLookup%2FairportDisplay.jsp%3FairportId%3D";
+      var b = "'%20and%20xpath%3D'%2F%2Ftbody%2F*%5Bcontains(.%2C%22";
+      var c = "%22)%5D'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+      var chartURL = [5];
+      chartURL[0] = 'http://vfrmap.com/api?req=map&type=vfrc&lat=';
+      chartURL[1] = '&lon=';
+      chartURL[2] = '&zoom=10&width=350&height=350';
+      chartURL[3] = 'http://vfrmap.com/?type=vfrc&lat=';
+      chartURL[4] = '&zoom=10';
+      var thisURL;
+
+      var convertCoord = function (latLongInDMS) {
+        //assumes NW hemisphere always
+        var latLongSplit = latLongInDMS.split("/");
+        //lat
+        var latDMS = latLongSplit[0].split("-");
+        var SDirect = latDMS[2].split(" ");
+        latDMS[2] = SDirect[0];
+        //long
+        var longDMS = latLongSplit[1].split("-");
+        var SDirect2 = longDMS[2].split(" ");
+        longDMS[2] = SDirect2[0];
+        //convert
+        var latLongInDeg = [2];
+        latLongInDeg[0] = parseInt(latDMS[0]) + parseInt(latDMS[1])/60 + parseInt(latDMS[2])/(60*60);
+        latLongInDeg[1] = -1*(parseInt(longDMS[0]) + parseInt(longDMS[1])/60 + parseInt(longDMS[2])/(60*60));
+        return latLongInDeg;
+      };
+
+      //GET data
+      var getData = function(idx, target, attribute) {
+        thisURL = a + $scope.airports[idx].ident + b + target + c;
+        $http.get(thisURL)
+        .then(function(response){
+          if (response.data.query.results){
+            if (response.data.query.results.tr.length > 1){
+              $scope.airports[idx][attribute] = response.data.query.results.tr[0].td[1];
+            }
+            else if (response.data.query.results.tr){
+              $scope.airports[idx][attribute] = response.data.query.results.tr.td[1];
+            }
+            else{
+              console.log("Could not retrieve data: " + attribute + " for " + $scope.airports[idx].ident);
+            }
+          }
+          if (attribute === 'latLong'){
+            var coordInDegrees = convertCoord($scope.airports[idx].latLong);
+            $scope.airports[idx].chartLink = chartURL[3] + coordInDegrees[0] + chartURL[1] + coordInDegrees[1] + chartURL[4];
+            $scope.airports[idx].chartURL = chartURL[0] + coordInDegrees[0] + chartURL[1] + coordInDegrees[1] + chartURL[2];
+          }
+        },function(err){
+          console.log("Error, could not get data for Airport:" + $scope.airports[idx].ident + "Attribute: " + attribute);
+        });
+      };
+
+      var a2 = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'https%3A%2F%2Fnfdc.faa.gov%2FnfdcApps%2Fservices%2FairportLookup%2FairportDisplay.jsp%3FairportId%3D";
+      var b2 ="'%20and%20xpath%3D'%2F%2Fdiv%5Bcontains(%40id%2C%22";
+      var c2 = "%22)%5D'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+
+      //GET comm and runways
+      var getData2 = function(idx, target, attribute) {
+        thisURL = a2 + $scope.airports[idx].ident + b2 + target + c2;
+        $http.get(thisURL)
+        .then(function(response){
+          $scope.airports[idx][attribute] = response.data.query.results.div;
+          if (attribute === 'comms'){
+            $scope.airports[idx][attribute] = response.data.query.results.div.table.tbody.tr;
+          }
+          console.log($scope.airports);
+        },function(err){
+          console.log("Error, could not get data for Airport:" + $scope.airports[idx].ident + "Attribute: " + attribute);
+        });
+      };
+
+      //Loop for all Airports
+      for (var index = 0; index < $scope.airports.length; index++){
+        //data
+        getData(index, encodeURIComponent("Latitude/Longitude"), "latLong");  //Latitude/Longitude
+        getData(index, encodeURIComponent("Elevation"), "elevation");
+        getData(index, encodeURIComponent("Variation"), "variation");
+        getData(index, encodeURIComponent("From city"), "city");
+        getData(index, encodeURIComponent("Section chart"), "sectional");
+        getData(index, encodeURIComponent("Time Zone"), "timezone");
+        getData(index, encodeURIComponent("Facility use"), "facilityUse");
+        getData(index, encodeURIComponent("FSS"), "fss");
+        //getData(index, encodeURIComponent("Attendance"), "attendance");
+        getData(index, encodeURIComponent("Wind Indicator"), "windIndicator");
+        getData(index, encodeURIComponent("Beacon"), "beacon");
+        getData(index, encodeURIComponent("Landing Fee"), "fee");
+        //comms
+        getData2(index, encodeURIComponent("communications"), "comms");
+        //runways
+        getData2(index, encodeURIComponent("runway"), "runways");
+        //nav chart
+      }
+
+
     };
-    //Make input
-    //vlaidate input
-    //submit request
-    //parse request
-    //display request, repeat based on number of input
-
-
-
-
 /*
     // Create new fltplan
     $scope.create = function (isValid) {
